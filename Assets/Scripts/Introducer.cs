f0ac4967c89c4d2064c288b913bdb62372c509b4
JonathanVanDyke <jfv213@gmail.com>
2020-05-23 18:05:17 -0400

    Initial commit

---
+++ Assets/Scripts/Introducer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Introducer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hi Jonathan");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
